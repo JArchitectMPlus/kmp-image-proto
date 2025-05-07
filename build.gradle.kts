e58e0d1020f3597df8276f86aa0893283865ea1b
JArchitectMPlus <jeff.akpunonu@materialplus.io>
2025-05-07 11:32:14 +1000

    Update dependencies and configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,117 @@
-implementation("com.soywiz.korlibs.korim:korim:2.2.0")
-implementation("io.github.markyav.drawbox:drawbox:1.3.1")
+import org.jetbrains.compose.ExperimentalComposeLibrary
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsCompose)
+    id("maven-publish")
+}
+
+group = "com.example"
+version = "1.0"
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "17"
+            }
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                @OptIn(ExperimentalComposeLibrary::class)
+                implementation(compose.components.resources)
+                implementation("com.soywiz.korlibs.korim:korim:2.2.0")
+                implementation("io.github.markyav.drawbox:drawbox:1.3.1")
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.bundles.koin)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("androidx.core:core-ktx:1.12.0")
+                implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+                implementation("androidx.activity:activity-compose:1.8.2")
+                implementation(compose.ui)
+                implementation(compose.uiGraphics)
+                implementation(compose.uiToolingPreview)
+                implementation(compose.material)
+                implementation(compose.material3)
+            }
+        }
+        val androidTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+        val iosMain by getting {
+            dependencies {
+                implementation(compose.ui)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                @OptIn(ExperimentalComposeLibrary::class)
+                implementation(compose.components.resources)
+            }
+        }
+        val iosTest by getting
+    }
+}
+
+android {
+    namespace = "com.example.imagemanipulator.android"
+    compileSdk = 34
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+
+    defaultConfig {
+        applicationId = "com.example.imagemanipulator.android"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+compose {
+    kotlinCompilerPlugin.set("1.5.3")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            from(components["release"])
+        }
+    }
+}
